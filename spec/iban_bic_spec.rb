e67a8cd76c83f95ddcbd414a11f8c279398f905c
Tomas Skogberg <tomas.skogberg@gmail.com>
2015-11-02 15:19:17 +0100

    Replace Ibanomat with Ibanizator
    We're making the assumption that the BIC that we get back from the Ibanizator is "good enough".
    
    There is more to read about this at:
    https://github.com/softwareinmotion/ibanizator/issues/10
    
    This reverts commit 558d56e1826301f9f9346b4cb76b821b6ff16cfa.

--- spec/iban_bic_spec.rb
+++ spec/iban_bic_spec.rb
@@ -2,39 +2,31 @@ require "spec_helper"
 require "banktools-de/iban_bic"
 
 describe BankTools::DE::IbanBicConverter, ".call" do
-  let(:blz) { "abc" }
-  let(:account) { "def" }
-
   it "returns the BIC and IBAN" do
-    allow(Ibanomat).to receive(:find).and_return(iban: "some iban", bic: "some bic", return_code: "00")
+    blz = "25050180"
+    account = "39370089"
 
     actual = BankTools::DE::IbanBicConverter.call(blz: blz, account: account)
 
-    expect(actual.iban).to eq("some iban")
-    expect(actual.bic).to eq("some bic")
+    expect(actual.iban).to eq("DE53250501800039370089")
+    expect(actual.bic).to eq("SPKHDE2HXXX")
   end
 
-  it "raises CouldNotConvertError on conversion errors" do
-    allow(Ibanomat).to receive(:find).and_return(return_code: "1")
+  it "raises CouldNotConvertIbanError on IBAN conversion errors" do
+    blz = "abc"
+    account = "def"
 
     expect {
       BankTools::DE::IbanBicConverter.call(blz: blz, account: account)
-    }.to raise_error(BankTools::DE::IbanBicConverter::CouldNotConvertError)
+    }.to raise_error(BankTools::DE::IbanBicConverter::CouldNotConvertIbanError)
   end
 
-  it "raises ServiceUnavailable when the service is down" do
-    allow(Ibanomat).to receive(:find).and_raise(RestClient::RequestTimeout)
+  it "raises CouldNotFindBicError on find BIC errors" do
+    blz = "21050171"
+    account = "12345678"
 
     expect {
       BankTools::DE::IbanBicConverter.call(blz: blz, account: account)
-    }.to raise_error(BankTools::DE::IbanBicConverter::ServiceUnavailable)
-  end
-
-  it "raises UnknownError on other errors" do
-    allow(Ibanomat).to receive(:find).and_raise("unknown error")
-
-    expect {
-      BankTools::DE::IbanBicConverter.call(blz: blz, account: account)
-    }.to raise_error(BankTools::DE::IbanBicConverter::UnknownError)
+    }.to raise_error(BankTools::DE::IbanBicConverter::CouldNotFindBicError)
   end
 end
